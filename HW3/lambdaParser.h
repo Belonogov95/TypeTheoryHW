313377e82bd5732261d6f33f2b94e6dd86241c40
Belonogov Ivan <Belonogov95@gmail.com>
2016-03-26 13:33:24 +0300

    hw3 done

---
+++ HW3/lambdaParser.h
@@ -0,0 +1,39 @@
+#include "main.h"
+
+#ifndef LABMDA_PARSER
+#define LABMDA_PARSER
+   
+
+
+
+struct LexicalAnalyzer {
+    string s;
+    string token;
+    int cur;
+    LexicalAnalyzer(string s);
+    void next();
+    string curToken();
+};
+
+struct Node {
+    string type;
+    vector < Node * > ch;
+    Node (string type);
+    Node (string type, Node * ch1);
+    Node (string type, Node * ch1, Node * ch2);
+    Node (string type, Node * ch1, Node * ch2, Node * ch3);
+};
+
+struct LambdaParser {
+    LexicalAnalyzer lex;
+    LambdaParser(string s);
+    Node * parseExp();
+    Node * parseApply();
+    Node * parseAtom();
+    Node * parseVar();
+    Node * parseCondition();
+};
+
+
+#endif
+
